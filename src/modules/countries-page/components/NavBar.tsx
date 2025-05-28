98b13e0d3b04108591526616ed2753ca4cc51a4f
Wizzaisha <ever.ramirezmahecha@gmail.com>
2025-05-27 22:25:36 -0500

    ajustes

--- src/modules/countries-page/components/NavBar.tsx
+++ src/modules/countries-page/components/NavBar.tsx
@@ -3,7 +3,7 @@ import ThemeComponent from "../../core/components/buttons/ThemeComponent";
 const NavBar = () => {
   return (
     <div className="w-full flex flex-row justify-between items-center py-5 sm:px-10 md:px-20 shadow-sm bg-primary">
-      <h1 className="text-preset2">Where is the world?</h1>
+      <h1 className="text-preset2">Where in the world?</h1>
 
       <ThemeComponent></ThemeComponent>
     </div>
